03354bbea4cb2accfc90e053a4a318179f48fcb6
Alej.OF <alej.of.19@gmail.com>
2024-07-10 20:50:05 -0400

    hito 3 parte 1

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Flan(models.Model):
+    flan_uuid = models.UUIDField()
+    name = models.CharField(max_length=64, default='')
+    description = models.TextField(default='')
+    image_url = models.URLField(default='')
+    slug = models.SlugField(default='')
+    is_private = models.BooleanField(default=True)
--- web/views.py
+++ web/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from .models import Flan
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    flans_pub = Flan.objects.filter(is_private = False)
+    context = {
+        'flans':flans_pub,
+    }
+    return render(request,'index.html',context)
 def about(request):
     return render(request,'about.html')
 def welcome(request):
-    return render(request,'welcome.html')
+    flans_priv = Flan.objects.filter(is_private = True)
+    context = {
+        'flans':flans_priv,
+    }
+    return render(request,'welcome.html',context)
